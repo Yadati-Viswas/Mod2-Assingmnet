4324e951102c1b8c9e3df502f258a1fe27d27623
Yadati-Viswas <67747462+Yadati-Viswas@users.noreply.github.com>
2020-12-26 15:08:43 +0530

    Update Script.js

--- site/Mod4-Assingment Answer/Script.js
+++ site/Mod4-Assingment Answer/Script.js
@@ -1,4 +1,4 @@
-var array=["Viswas","John","jay","January","Datta","Sai","Jayanth"];
+var array=["Viswas","John","jay","January","Datta","Sai","jayanth"];
 for(var i=0;i<array.length;i++){
 	if(array[i].startsWith("j")|| array[i].startsWith("J")){
 		console.log("Good Bye "+array[i]);
